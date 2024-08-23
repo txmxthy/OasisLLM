1021356309bb3d8b953835a04bfd9d0f182e3c37
Timothy <50641082+txmxthy@users.noreply.github.com>
2024-08-23 15:14:04 +1200

    test case for linter

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def gpu_status():
         print(f"Cuda GPU: {torch.cuda.get_device_name(0)} is Available.")
     else:
         print("Cuda GPU Not Available.")
+
+
     input("Press Enter to continue...")
 
 
@@ -21,32 +23,48 @@ def configuration():
         - Number of files in ingestion directory
         - Chunk Size3
         - Chunk Overlap
+
     :return:
     """
+
     pass
 
 
 def main(first_run=None):
     """
-    Main function for the Oasis Toolkit
+    Main    function for the Oasis Toolkit
     :return:
+
+
     """
     while True:
+
         if not first_run:
             # Clear the console
             print("\033[H\033[J")
         first_run = False
+
+
         print_header("The Oasis Toolkit")
+
         functions = {
             "Document Ingestion": ingest_main,
             "Query": oasis_main,
+
             "Config": configuration,
-            "GPU Status": gpu_status,
+
+            "GPU Status":           gpu_status,
         }
         # Call the select method and run the selected function
-        choice = select([*functions.keys()])
+
+
+
+        choice =        select([*functions.keys()])
         functions[choice]()
 
 
 if __name__ == '__main__':
+
+
+
     main(first_run=True)
